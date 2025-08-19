a317c8a8931e5029f4dc4a4a4721cb183e0b6392
vikashjaingarg04 <ankitjainkherijalab@gmail.com>
2025-08-19 17:24:27 +0530

    Initial Commit

---
+++ AppAssignment/AppAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAssignmentApp.swift
+//  AppAssignment
+//
+//  Created by Ankit Jain on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AppAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
